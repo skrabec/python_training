aa598e6d63a5989405c4b7b5207f1d6093148a09
skrabec <skrabets@gmail.com>
2023-06-08 16:31:11 +0200

    Fix variables

--- test_add_contact.py
+++ test_add_contact.py
@@ -4,12 +4,16 @@ from selenium.webdriver.support.ui import Select
 from selenium.webdriver.firefox.webdriver import WebDriver
 import unittest
 from contact import Contact
+
+
 def is_alert_present(wd):
     try:
         wd.switch_to_alert().text
         return True
     except:
         return False
+
+
 class test_add_contact(unittest.TestCase):
     def setUp(self):
         self.wd = WebDriver()
@@ -18,6 +22,7 @@ class test_add_contact(unittest.TestCase):
 
     def open_home_page(self, wd):
         wd.get("https://localhost/addressbook/")
+
     def login(self, wd, username, password):
         wd.find_element(By.NAME, "user").click()
         wd.find_element(By.NAME, "user").clear()
@@ -25,8 +30,10 @@ class test_add_contact(unittest.TestCase):
         wd.find_element(By.NAME, "pass").clear()
         wd.find_element(By.NAME, "pass").send_keys(password)
         wd.find_element(By.XPATH, "//input[@value='Login']").click()
+
     def open_contacts_page(self, wd):
         wd.find_element(By.LINK_TEXT, "add new").click()
+
     def create_contact(self, wd, contact):
         # fill contact form
         wd.find_element(By.NAME, "firstname").click()
@@ -75,19 +82,14 @@ class test_add_contact(unittest.TestCase):
         wd.find_element(By.NAME, "homepage").clear()
         wd.find_element(By.NAME, "homepage").send_keys(contact.homepage)
         Select(wd.find_element(By.NAME, "bday")).select_by_visible_text(contact.bday)
-        wd.find_element(By.XPATH, "//option[@value='7']").click()
-        wd.find_element(By.NAME, "bmonth").click()
         Select(wd.find_element(By.NAME, "bmonth")).select_by_visible_text(contact.bmonth)
-        wd.find_element(By.XPATH, "//option[@value='July']").click()
         wd.find_element(By.NAME, "byear").click()
         wd.find_element(By.NAME, "byear").clear()
         wd.find_element(By.NAME, "byear").send_keys(contact.byear)
         wd.find_element(By.NAME, "aday").click()
         Select(wd.find_element(By.NAME, "aday")).select_by_visible_text(contact.aday)
-        wd.find_element(By.XPATH, "//select[3]/option[12]").click()
         wd.find_element(By.NAME, "amonth").click()
         Select(wd.find_element(By.NAME, "amonth")).select_by_visible_text(contact.amonth)
-        wd.find_element(By.XPATH, "//select[4]/option[11]").click()
         wd.find_element(By.NAME, "ayear").click()
         wd.find_element(By.NAME, "ayear").clear()
         wd.find_element(By.NAME, "ayear").send_keys(contact.ayear)
@@ -101,22 +103,32 @@ class test_add_contact(unittest.TestCase):
         wd.find_element(By.NAME, "notes").clear()
         wd.find_element(By.NAME, "notes").send_keys(contact.notes)
         # submit form
-        wd.find_element(By.XPATH, "//div[@id='content']/form/input[21]").click()
+        wd.find_element(By.NAME, "submit").click()
+
     def return_to_home_page(self, wd):
         wd.find_element(By.LINK_TEXT, "home page").click()
+
     def logout(self, wd):
         wd.find_element(By.LINK_TEXT, "Logout").click()
+
     def test_add_contact(self):
         wd = self.wd
         self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
         self.open_contacts_page(wd)
-        self.create_contact(wd, Contact(firstname="fn", middlename="mn", lastname="ln", nickname="nn", title="ttl", company="cmp", address="adr1", home="111111", mobile="222222", work="333333", fax="444444",
-                            email="mail@mail.com", email2="mail2@mail.com", email3="mail3@mail.com", homepage="www.hopg.com", bday="7", bmonth="July", byear="1977",
-                            aday="10", amonth="October", ayear="2010", address2="adr2", phone2="555555", notes="test"))
+        self.create_contact(wd, Contact(firstname="fn", middlename="mn", lastname="ln", nickname="nn", title="ttl",
+                                        company="cmp", address="adr1", home="111111", mobile="222222", work="333333",
+                                        fax="444444",
+                                        email="mail@mail.com", email2="mail2@mail.com", email3="mail3@mail.com",
+                                        homepage="www.hopg.com", bday="7", bmonth="July", byear="1977",
+                                        aday="10", amonth="October", ayear="2010", address2="adr2", phone2="555555",
+                                        notes="test"))
         self.return_to_home_page(wd)
         self.logout(wd)
+
     def tearDown(self):
         self.wd.quit()
+
+
 if __name__ == "__main__":
     unittest.main()
