f0ad0a549b35385c4dc6862bc5db6dccf49e9488
skrabec <skrabets@gmail.com>
2023-06-09 14:20:27 +0200

    Create fixture and move methods there

--- test_add_group.py
+++ test_add_group.py
@@ -1,75 +1,26 @@
 # -*- coding: utf-8 -*-
-from selenium.webdriver.common.by import By
-from selenium.webdriver.firefox.webdriver import WebDriver
-import unittest
+
+import pytest
+
+from application import Application
 from group import Group
 
 
-def is_alert_present(wd):
-    try:
-        wd.switch_to_alert().text
-        return True
-    except:
-        return False
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
 
 
-class test_add_group(unittest.TestCase):
-    def setUp(self):
-        self.wd = WebDriver()
-        self.wd.implicitly_wait(60)
+def test_add_group(app):
+    app.login("admin", "secret")
+    app.create_new_group(Group("test", "test", "teeest"))
+    app.logout()
 
 
-    def logout(self, wd):
-        wd.find_element(By.LINK_TEXT, "Logout").click()
+def test_add_empy_group(app):
+    app.login("admin", "secret")
+    app.create_new_group(Group("", "", ""))
+    app.logout()
 
-    def return_to_groups_page(self, wd):
-        wd.find_element(By.LINK_TEXT, "group page").click()
-
-    def create_new_group(self, wd, group):
-        self.open_groups_page(wd)
-        wd.find_element(By.NAME, "new").click()
-        wd.find_element(By.NAME, "group_name").click()
-        wd.find_element(By.NAME, "group_name").clear()
-        wd.find_element(By.NAME, "group_name").send_keys(group.name)
-        wd.find_element(By.NAME, "group_header").click()
-        wd.find_element(By.NAME, "group_header").clear()
-        wd.find_element(By.NAME, "group_header").send_keys(group.header)
-        wd.find_element(By.NAME, "group_footer").click()
-        wd.find_element(By.NAME, "group_footer").clear()
-        wd.find_element(By.NAME, "group_footer").send_keys(group.footer)
-        wd.find_element(By.NAME, "submit").click()
-        self.return_to_groups_page(wd)
-
-    def open_groups_page(self, wd):
-        wd.find_element(By.LINK_TEXT, "groups").click()
-
-    def login(self, wd, username, password):
-        self.open_home_page(wd)
-        wd.find_element(By.NAME, "user").click()
-        wd.find_element(By.NAME, "user").clear()
-        wd.find_element(By.NAME, "user").send_keys(username)
-        wd.find_element(By.NAME, "pass").clear()
-        wd.find_element(By.NAME, "pass").send_keys(password)
-        wd.find_element(By.XPATH, "//input[@value='Login']").click()
-
-    def open_home_page(self, wd):
-        wd.get("https://localhost/addressbook/")
-
-    def test_add_group(self):
-        wd = self.wd
-        self.login(wd, "admin", "secret")
-        self.create_new_group(wd, Group("test", "test", "teeest"))
-        self.logout(wd)
-
-    def test_add_empy_group(self):
-        wd = self.wd
-        self.login(wd, "admin", "secret")
-        self.create_new_group(wd, Group("", "", ""))
-        self.logout(wd)
-
-    def tearDown(self):
-        self.wd.quit()
-
-
-if __name__ == "__main__":
-    unittest.main()
