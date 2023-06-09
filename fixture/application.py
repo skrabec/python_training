95811caa509bff98cdb5f381ed5a04e311a1c1a0
skrabec <skrabets@gmail.com>
2023-06-09 15:42:04 +0200

    Add fixture for contacts

--- fixture/application.py
+++ fixture/application.py
@@ -3,6 +3,7 @@ from selenium.webdriver.firefox.webdriver import WebDriver
 
 from fixture.group import GroupHelper
 from fixture.session import SessionHelper
+from fixture.contact import ContactHelper
 
 
 class Application:
@@ -12,6 +13,7 @@ class Application:
         self.wd.implicitly_wait(60)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
+        self.contact = ContactHelper(self)
 
     def open_home_page(self):
         wd = self.wd
