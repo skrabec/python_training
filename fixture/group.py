8ea816e8b8d20fbf6d52ea4d7b487e974265ba48
skrabec <skrabets@gmail.com>
2023-06-17 14:05:08 +0200

    Optimized groups input

--- fixture/group.py
+++ fixture/group.py
@@ -18,40 +18,56 @@ class GroupHelper:
         wd = self.app.wd
         self.open_groups_page()
         wd.find_element(By.NAME, "new").click()
-        wd.find_element(By.NAME, "group_name").click()
-        wd.find_element(By.NAME, "group_name").clear()
-        wd.find_element(By.NAME, "group_name").send_keys(group.name)
-        wd.find_element(By.NAME, "group_header").click()
-        wd.find_element(By.NAME, "group_header").clear()
-        wd.find_element(By.NAME, "group_header").send_keys(group.header)
-        wd.find_element(By.NAME, "group_footer").click()
-        wd.find_element(By.NAME, "group_footer").clear()
-        wd.find_element(By.NAME, "group_footer").send_keys(group.footer)
+        self.fill_group_form(group)
         wd.find_element(By.NAME, "submit").click()
         self.return_to_groups_page()
 
     def delete_first_group(self):
         wd = self.app.wd
         self.open_groups_page()
-        wd.find_element(By.NAME, "selected[]").click()
+        self.select_first_group()
         wd.find_element(By.NAME, "delete").click()
         self.return_to_groups_page()
 
     def edit_first_group(self, group):
         wd = self.app.wd
         self.open_groups_page()
-        wd.find_element(By.NAME, "selected[]").click()
+        self.select_first_group()
         wd.find_element(By.NAME, "edit").click()
-        wd.find_element(By.NAME, "group_name").click()
-        wd.find_element(By.NAME, "group_name").clear()
-        wd.find_element(By.NAME, "group_name").send_keys(group.name)
-        wd.find_element(By.NAME, "group_header").click()
-        wd.find_element(By.NAME, "group_header").clear()
-        wd.find_element(By.NAME, "group_header").send_keys(group.header)
-        wd.find_element(By.NAME, "group_footer").click()
-        wd.find_element(By.NAME, "group_footer").clear()
-        wd.find_element(By.NAME, "group_footer").send_keys(group.footer)
+        self.fill_group_form(group)
         wd.find_element(By.NAME, "update").click()
         self.return_to_groups_page()
 
+    def fill_group_form(self, group):
+        self.change_field_value("group_name", group.name)
+        self.change_field_value("group_header", group.header)
+        self.change_field_value("group_footer", group.footer)
 
+    def change_field_value(self, field_name, text):
+        wd = self.app.wd
+        if text is not None:
+            wd.find_element(By.NAME, field_name).click()
+            wd.find_element(By.NAME, field_name).clear()
+            wd.find_element(By.NAME, field_name).send_keys(text)
+
+    def select_first_group(self):
+        wd = self.app.wd
+        wd.find_element(By.NAME, "selected[]").click()
+
+    def modify_first_group_name(self, new_group_data):
+        wd = self.app.wd
+        self.open_groups_page()
+        self.select_first_group()
+        wd.find_element(By.NAME, "edit").click()
+        self.fill_group_form(new_group_data)
+        wd.find_element(By.NAME, "update").click()
+        self.return_to_groups_page()
+
+    def modify_first_group_header(self, new_group_data):
+        wd = self.app.wd
+        self.open_groups_page()
+        self.select_first_group()
+        wd.find_element(By.NAME, "edit").click()
+        self.fill_group_form(new_group_data)
+        wd.find_element(By.NAME, "update").click()
+        self.return_to_groups_page()
--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -5,3 +5,15 @@ def test_add_group(app):
     app.session.login("admin", "secret")
     app.group.edit_first_group(Group("test", "test", "teeest"))
     app.session.logout()
+
+
+def test_modify_first_group_name(app):
+    app.session.login("admin", "secret")
+    app.group.modify_first_group_name(Group("modified_group_name"))
+    app.session.logout()
+
+
+def test_modify_first_group_header(app):
+    app.session.login("admin", "secret")
+    app.group.modify_first_group_header(Group(header="modified_group_header"))
+    app.session.logout()
